ad40648a7f8df2954539445882f4addf45a32c50
ccprek <c@psioniclabs.com>
2014-02-09 16:43:31 +0000

    Slightly working stack

--- linecook.py
+++ linecook.py
@@ -2,16 +2,26 @@ import serial
 import json
 import httplib2
 import datetime
+import time
 
 import lookup
+import oven
 
 device = "/dev/ttyACM0"
 scanner = serial.Serial(device)
 
 while(1):
     product = scanner.readline()
+    print "Scanned UPC: " + product
     for step in lookup.cook_data(lookup.form_upc_url(product)):
-        print step
+        print "Starting cook step " + step
+        (cook_json, cook_path), (check_data, check_path) = oven.parse_instruction(step)
+        oven.oven_set(cook_path, cook_json)
+        print "check_data: " + check_data
+        print "check_path: " + check_path
+        while (oven.oven_get(check_path) != check_data):
+            print "Waiting for " + check_path + " == " + check_data
+            time.sleep(10)
 
 scanner.close()
     
--- oven.py
+++ oven.py
@@ -1,12 +1,15 @@
 import json
 import httplib2
+import datetime
 
 oven_ip = '127.0.0.1'
 oven_port = '8080'
 oven_state = {0: "No Mode", 1: "Preheat", 2: "Convection Bake Preheat"}
 
-json_headers = "Content-Type: application/json"
+json_headers = {"Content-Type": "application/json"}
 default_data = {"cookHours": 0, "cookMinutes": 0, "mode": 0, "cookTemperature": 100}
+
+mode = 18
 
 def oven_get(path):
     url = "http://" + oven_ip + ":" + oven_port + "/" + path
@@ -28,5 +31,36 @@ def oven_set(path, json_data):
     if (resp.status == 200):
         return True
     else:
+        print url
+        print resp.status
         return False
 
+def parse_instruction(inst):
+    cookHours = 0
+    cookMinutes = 0
+    mode = 0
+    cookTemperature = 100
+    next_state = 0
+    start_path = ""
+    params = inst.split(",")
+    for param in params:
+        state = param[0]
+        value = param[1:]
+        if state == 'P':
+            cookTemperature = int(value)
+            mode = 18
+            start_path = "upperOven/cookMode"
+            next_path = "upperOven/displayTemperature"
+            next_state = value
+        elif state == 'B':
+            cookTemperature = int(value)
+            mode = 18
+            start_path = "upperOven/cookMode"
+            next_path = "upperOven/cookTimeRemaining"
+            next_state = 0
+        elif state == 'T':
+            cookHours, cookMinutes = divmod(int(value), 60)
+
+    start_data = {"cookHours": cookHours, "cookMinutes": cookMinutes, "mode": mode, "cookTemperature": cookTemperature}
+    json_start = json.dumps(start_data)
+    return (json_start, start_path), (next_state, next_path) 
