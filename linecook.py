a563f39bf3a0ce69ea46b33012a01887e8e95cf3
ccprek <c@psioniclabs.com>
2014-02-09 14:15:09 +0000

    Forgot to commit a couple

--- linecook.py
+++ linecook.py
@@ -1,11 +1,17 @@
 import serial
+import json
+import httplib2
+import datetime
+
+import lookup
 
 device = "/dev/ttyACM0"
 scanner = serial.Serial(device)
 
 while(1):
     product = scanner.readline()
-    print product
+    for step in lookup.cook_data(lookup.form_upc_url(product)):
+        print step
 
 scanner.close()
     
